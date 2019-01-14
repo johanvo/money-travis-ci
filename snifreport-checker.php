ed7abd5e3f0fd6e3a3a44387143d9a504c9bf54c
Johan van Overbeeke <johan@omniboxx.nl>
2019-01-14 08:55:37 +0100

    Output style violations to log

--- snifreport-checker.php
+++ snifreport-checker.php
@@ -7,12 +7,27 @@ if (!file_exists($inputFile)) {
 }
 
 $xml = new SimpleXMLElement(file_get_contents($inputFile));
-$errors = $xml->xpath("//error[@severity='error']");
+$filesWithError = $xml->xpath("//error[@severity='error']/parent::file");
 
-if (count($errors)) {
-	echo 'PSR2 style ORDERS MUST BE OBAYED AT ALL TIMES' . PHP_EOL;
+if (count($filesWithError) > 0) {
+	echo PHP_EOL . 'FAILURE: ' . PHP_EOL;
+
+	foreach ($filesWithError as $fileWithError) {
+		$filename = $fileWithError['name'];
+		$filename = str_replace(__DIR__, '', $filename);
+		$errors = $fileWithError->xpath("error[@severity='error']");
+
+		foreach ($errors as $error) {
+			$error = (array)$error;
+			$message = "{$filename}:{$error['@attributes']['line']}:{$error['@attributes']['column']} "
+				. $error['@attributes']['message'] . PHP_EOL;
+			echo $message;
+		}
+	}
+	echo PHP_EOL . 'PSR2 style ORDERS MUST BE OBAYED AT ALL TIMES' . PHP_EOL;
+
 	exit(1);
 
-} else {
-	echo 'Well done citizin' . PHP_EOL;
 }
+
+echo 'Well done citizin' . PHP_EOL;
